974b07f3527eb4e795af653f96ddd1e3119ac6da
Manfred Touron <m@42.am>
2019-08-05 22:10:44 +0200

    feat: cli now accepts file as argument

--- main.go
+++ main.go
@@ -1,24 +1,42 @@
 package main // import "moul.io/cryptoguess"
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
-	"io"
+	"io/ioutil"
+	"log"
 	"os"
 
 	"moul.io/cryptoguess/cryptoguess"
 )
 
 func main() {
-	in := bufio.NewReader(os.Stdin)
-	input := []byte{}
-	for {
-		chunk, err := in.ReadByte()
-		if err == io.EOF {
-			break
-		}
-		input = append(input, chunk)
+	if err := guess(); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
 	}
-	question := cryptoguess.New(input)
+}
+
+func guess() error {
+	flag.Parse()
+	var data []byte
+	var err error
+	switch flag.NArg() {
+	case 0:
+		data, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			return err
+		}
+	case 1:
+		data, err = ioutil.ReadFile(flag.Arg(0))
+		if err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("input must be from stdin or file")
+	}
+
+	question := cryptoguess.New(data)
 	fmt.Println(question)
+	return nil
 }
