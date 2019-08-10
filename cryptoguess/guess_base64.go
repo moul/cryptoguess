d354a4e83a65c3f76563ac1e3d224ed4c6d90cc1
Manfred Touron <m@42.am>
2019-08-10 11:20:12 +0200

    feat: add base64 decoder

---
+++ cryptoguess/guess_base64.go
@@ -0,0 +1,35 @@
+package cryptoguess
+
+import (
+	"encoding/base64"
+)
+
+func init() {
+	AvailableExperiments = append(AvailableExperiments, NewBASE64Block)
+}
+
+type BASE64Block struct{ *baseExperiment }
+
+func runBASE64Block(exp Experiment) []Result {
+	results := []Result{}
+
+	data, err := base64.StdEncoding.DecodeString(string(exp.Input()))
+	result := &baseResult{
+		exp:  exp,
+		data: data,
+		err:  err,
+	}
+	results = append(results, result)
+	// FIXME: recursively call other parsers with prefix=base64-encoded (like multiaddr)
+	return results
+}
+
+func NewBASE64Block(input []byte) Experiment {
+	return &BASE64Block{
+		baseExperiment: &baseExperiment{
+			input: input,
+			name:  "BASE64 encoded data",
+			run:   runBASE64Block,
+		},
+	}
+}
